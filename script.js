23421bd60659a55dfd8125c56ac2e347f555b578
kristian-a11y <kristian@walker-adams.co.uk>
2025-12-18 17:24:06 +0000

    Update script.js
    Add menu screen logic

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ function checkAnswer(answer) {
     feedback.style.color = "red";
   }
 }
+const startBtn = document.getElementById("start-btn");
+const menuScreen = document.getElementById("menu-screen");
+const quizScreen = document.getElementById("quiz-screen");
+
+startBtn.addEventListener("click", () => {
+  menuScreen.hidden = true;
+  quizScreen.hidden = false;
+});
