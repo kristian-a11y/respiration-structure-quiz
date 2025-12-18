6c087149b5457d2ab7ad7975140b3fe9e6eacb83
kristian-a11y <kristian@walker-adams.co.uk>
2025-12-18 17:32:49 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function checkAnswer(answer) {
-  const feedback = document.getElementById("feedback");
-  if (answer === "Citrate") {
-    feedback.textContent = "Correct!";
-    feedback.style.color = "green";
-  } else {
-    feedback.textContent = "Incorrect. Try again.";
-    feedback.style.color = "red";
-  }
-}
 const startBtn = document.getElementById("start-btn");
 const menuScreen = document.getElementById("menu-screen");
 const quizScreen = document.getElementById("quiz-screen");
